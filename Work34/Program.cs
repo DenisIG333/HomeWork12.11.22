30e6ae644bb1e9ce252d00e10725ea2da1018d51
DenisIG333 <ignatovdnc@inbox.ru>
2022-11-16 23:51:10 +0300

    Домашнее задание

---
+++ Work34/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int Arraysize = 8;
+int[] numbers = new int[Arraysize];
+RandomArray(numbers);
+Console.WriteLine("массив: ");
+Print(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"-> {count}");
+
+void RandomArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void Print(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ Work36/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> */
+
+
+int size = 9;
+int[] numbers = new int[size];
+RandomNumbers(numbers);
+Console.WriteLine("массив: ");
+Print(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i +=2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"сумма элементов cтоящих на нечётных позициях = {sum}");
+
+void RandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+
+void Print(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
